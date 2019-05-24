3696b3066ada17955ea2597aade1464af72d8fe0
Fernando Munoz <munozfernando92@yahoo.com>
2019-05-23 21:37:02 -0600

    enemies spawn in waves and move along waypoints

---
+++ Assets/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float _speed = 10f;
+    private Transform _target;
+    private int _pointIndex = 0;
+
+    public void Start()
+    {
+        _target = Waypoints.waypoints[0];
+    }
+
+    public float Speed
+    {
+        get { return _speed;}
+        set {_speed = value; }
+    }
+    private void Update()
+    {
+        Vector3 dir = _target.position - transform.position;
+        transform.Translate(dir.normalized * _speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, _target.position) <= .5f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if(_pointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(this);
+            return;
+        }
+        _pointIndex++;
+        _target = Waypoints.waypoints[_pointIndex];
+    }
+}
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = .0125f;
+    public float countdown = 2f;
+    public Text waveCountdownText;
+
+    private int waveNumber = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = countdown.ToString("0");
+    }
+
+    IEnumerator SpawnWave()
+    {
+        countdown = timeBetweenWaves;
+        waveNumber++;
+
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.25f);
+        }
+        
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
